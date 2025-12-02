8176b75e0157f89be5c328987af44bafb8042d59
Ali Baran Demir <alibarandemir798@gmail.com>
2025-12-02 20:16:48 +0300

    fix

--- src/main/java/com/alibarandemir/bankappservice/entity/Account.java
+++ src/main/java/com/alibarandemir/bankappservice/entity/Account.java
@@ -2,8 +2,6 @@ package com.alibarandemir.bankappservice.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
